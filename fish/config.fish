9b20ee7a3c37655341f9e4db381cf7236fff5abd
Weston Hullander <whulland@Westons-MacBook-Pro.local>
2025-04-07 20:08:52 -0500

    tweaks

--- fish/config.fish
+++ fish/config.fish
@@ -35,7 +35,7 @@ export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 
-export EDITOR="nvim"
+export EDITOR="hx"
 
 # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
