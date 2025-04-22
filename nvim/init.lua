75ce13c6cb30e9d8c0d41036f62916c3eef3306d
Weston Hullander <whulland@ip-192-168-30-47.us-gov-west-1.compute.internal>
2025-04-22 14:18:57 -0500

    add golf

--- nvim/init.lua
+++ nvim/init.lua
@@ -1234,6 +1234,7 @@ require("lazy").setup({
 			vim.g.loaded_netrwPlugin = 1
 		end,
 	},
+	"vuciv/golf",
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
