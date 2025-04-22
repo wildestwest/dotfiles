0ccd0766ff27e72a52e9f42aaad819870ce77ad8
Weston Hullander <whulland@ip-192-168-30-47.us-gov-west-1.compute.internal>
2025-04-22 17:41:42 -0500

    more nvim tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -147,6 +147,11 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "quik fix prev" })
 -- vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz', { desc = 'quik fix lnext' })
 -- vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz', { desc = 'quik fix lnext' })
 
+-- easier stuff
+vim.keymap.set("n", "<BS>b", "<cmd>bc<CR>", { desc = "close buffer" })
+vim.keymap.set("n", "<BS>x", "<cmd>qa<CR>", { desc = "quit all" })
+vim.keymap.set("n", "<BS>w", "<cmd>wa<CR>", { desc = "write all" })
+
 -- replace word cursor was on
 vim.keymap.set(
 	"n",
@@ -182,6 +187,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		if vim.fn.argv(0) == "" then
+			require("snacks.picker").smart()
+		end
 	end,
 })
 
@@ -671,7 +684,15 @@ require("lazy").setup({
 			local servers = {
 				-- clangd = {},
 				gopls = {},
-				basedpyright = {},
+				basedpyright = {
+					settings = {
+						basedpyright = {
+							analysis = {
+								typeCheckingMode = "standard",
+							},
+						},
+					},
+				},
 				flake8 = {},
 				ruff = {},
 				rust_analyzer = { enabled = false },
@@ -1027,74 +1048,74 @@ require("lazy").setup({
 			opts = {},
 		},
 	},
-	{
-
-		{ -- Linting
-			"mfussenegger/nvim-lint",
-			event = { "BufReadPre", "BufNewFile" },
-			config = function()
-				local lint = require("lint")
-				lint.linters_by_ft = {
-					markdown = { "markdownlint" },
-				}
-
-				-- To allow other plugins to add linters to require('lint').linters_by_ft,
-				-- instead set linters_by_ft like this:
-				-- lint.linters_by_ft = lint.linters_by_ft or {}
-				-- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-				--
-				-- However, note that this will enable a set of default linters,
-				-- which will cause errors unless these tools are available:
-				-- {
-				--   clojure = { "clj-kondo" },
-				--   dockerfile = { "hadolint" },
-				--   inko = { "inko" },
-				--   janet = { "janet" },
-				--   json = { "jsonlint" },
-				--   markdown = { "vale" },
-				--   rst = { "vale" },
-				--   ruby = { "ruby" },
-				--   terraform = { "tflint" },
-				--   text = { "vale" }
-				-- }
-				--
-				-- You can disable the default linters by setting their filetypes to nil:
-				-- lint.linters_by_ft['clojure'] = nil
-				-- lint.linters_by_ft['dockerfile'] = nil
-				-- lint.linters_by_ft['inko'] = nil
-				-- lint.linters_by_ft['janet'] = nil
-				-- lint.linters_by_ft['json'] = nil
-				-- lint.linters_by_ft['markdown'] = nil
-				-- lint.linters_by_ft['rst'] = nil
-				-- lint.linters_by_ft['ruby'] = nil
-				-- lint.linters_by_ft['terraform'] = nil
-				-- lint.linters_by_ft['text'] = nil
-
-				-- Create autocommand which carries out the actual linting
-				-- on the specified events.
-				local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-				vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-					group = lint_augroup,
-					callback = function()
-						-- Only run the linter in buffers that you can modify in order to
-						-- avoid superfluous noise, notably within the handy LSP pop-ups that
-						-- describe the hovered symbol using Markdown.
-						if vim.opt_local.modifiable:get() then
-							lint.try_lint()
-						end
-					end,
-				})
-			end,
-		},
-	},
+	-- {
+	--
+	-- 	{ -- Linting
+	-- 		"mfussenegger/nvim-lint",
+	-- 		event = { "BufReadPre", "BufNewFile" },
+	-- 		config = function()
+	-- 			local lint = require("lint")
+	-- 			lint.linters_by_ft = {
+	-- 				markdown = { "markdownlint" },
+	-- 			}
+	--
+	-- 			-- To allow other plugins to add linters to require('lint').linters_by_ft,
+	-- 			-- instead set linters_by_ft like this:
+	-- 			-- lint.linters_by_ft = lint.linters_by_ft or {}
+	-- 			-- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+	-- 			--
+	-- 			-- However, note that this will enable a set of default linters,
+	-- 			-- which will cause errors unless these tools are available:
+	-- 			-- {
+	-- 			--   clojure = { "clj-kondo" },
+	-- 			--   dockerfile = { "hadolint" },
+	-- 			--   inko = { "inko" },
+	-- 			--   janet = { "janet" },
+	-- 			--   json = { "jsonlint" },
+	-- 			--   markdown = { "vale" },
+	-- 			--   rst = { "vale" },
+	-- 			--   ruby = { "ruby" },
+	-- 			--   terraform = { "tflint" },
+	-- 			--   text = { "vale" }
+	-- 			-- }
+	-- 			--
+	-- 			-- You can disable the default linters by setting their filetypes to nil:
+	-- 			-- lint.linters_by_ft['clojure'] = nil
+	-- 			-- lint.linters_by_ft['dockerfile'] = nil
+	-- 			-- lint.linters_by_ft['inko'] = nil
+	-- 			-- lint.linters_by_ft['janet'] = nil
+	-- 			-- lint.linters_by_ft['json'] = nil
+	-- 			-- lint.linters_by_ft['markdown'] = nil
+	-- 			-- lint.linters_by_ft['rst'] = nil
+	-- 			-- lint.linters_by_ft['ruby'] = nil
+	-- 			-- lint.linters_by_ft['terraform'] = nil
+	-- 			-- lint.linters_by_ft['text'] = nil
+	--
+	-- 			-- Create autocommand which carries out the actual linting
+	-- 			-- on the specified events.
+	-- 			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+	-- 			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	-- 				group = lint_augroup,
+	-- 				callback = function()
+	-- 					-- Only run the linter in buffers that you can modify in order to
+	-- 					-- avoid superfluous noise, notably within the handy LSP pop-ups that
+	-- 					-- describe the hovered symbol using Markdown.
+	-- 					if vim.opt_local.modifiable:get() then
+	-- 						lint.try_lint()
+	-- 					end
+	-- 				end,
+	-- 			})
+	-- 		end,
+	-- 	},
+	-- },
 	{
 		"folke/flash.nvim",
 		event = "VeryLazy",
 		opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<enter>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
@@ -1179,6 +1200,7 @@ require("lazy").setup({
 			open_mapping = [[<c-t>]],
 			direction = "vertical",
 			autochdir = true,
+			shell = "nu",
 		},
 	},
 	{
@@ -1203,26 +1225,20 @@ require("lazy").setup({
 		keys = {
 			-- 👇 in this section, choose your own keymappings!
 			{
-				"<leader>-",
+				"<leader>e",
 				mode = { "n", "v" },
 				"<cmd>Yazi<cr>",
 				desc = "Open yazi at the current file",
 			},
 			{
-				-- Open in the current working directory
-				"<leader>cw",
-				"<cmd>Yazi cwd<cr>",
-				desc = "Open the file manager in nvim's working directory",
-			},
-			{
-				"<c-up>",
+				"<leader>E",
 				"<cmd>Yazi toggle<cr>",
 				desc = "Resume the last yazi session",
 			},
 		},
 		opts = {
 			-- if you want to open yazi instead of netrw, see below for more info
-			open_for_directories = true,
+			open_for_directories = false,
 			keymaps = {
 				show_help = "<f1>",
 			},
