98c404d224bbda9b6edbd5c50110d4dd4acb20d9
Weston Hullander <whulland@ip-192-168-30-47.us-gov-west-1.compute.internal>
2025-05-19 15:39:48 -0500

    add learning vim stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -1150,15 +1150,15 @@ require("lazy").setup({
 			end,
 		},
 	},
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		opts = {},
-  -- stylua: ignore
-  keys = {
-    { "<enter>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-  },
-	},
+	-- {
+	-- 	"folke/flash.nvim",
+	-- 	event = "VeryLazy",
+	-- 	opts = {},
+	--  -- stylua: ignore
+	--  keys = {
+	--    { "<enter>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+	--  },
+	-- },
 	{
 		{
 			"ThePrimeagen/harpoon",
@@ -1289,6 +1289,44 @@ require("lazy").setup({
 		end,
 	},
 	"vuciv/golf",
+	"ThePrimeagen/vim-be-good",
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
+	},
+
+	{
+		"tris203/precognition.nvim",
+		--event = "VeryLazy",
+		opts = {
+			-- startVisible = true,
+			-- showBlankVirtLine = true,
+			-- highlightColor = { link = "Comment" },
+			-- hints = {
+			--      Caret = { text = "^", prio = 2 },
+			--      Dollar = { text = "$", prio = 1 },
+			--      MatchingPair = { text = "%", prio = 5 },
+			--      Zero = { text = "0", prio = 1 },
+			--      w = { text = "w", prio = 10 },
+			--      b = { text = "b", prio = 9 },
+			--      e = { text = "e", prio = 8 },
+			--      W = { text = "W", prio = 7 },
+			--      B = { text = "B", prio = 6 },
+			--      E = { text = "E", prio = 5 },
+			-- },
+			-- gutterHints = {
+			--     G = { text = "G", prio = 10 },
+			--     gg = { text = "gg", prio = 9 },
+			--     PrevParagraph = { text = "{", prio = 8 },
+			--     NextParagraph = { text = "}", prio = 8 },
+			-- },
+			-- disabled_fts = {
+			--     "startify",
+			-- },
+		},
+	},
 	-- lazy.nvim:
 	{
 		"jake-stewart/multicursor.nvim",
